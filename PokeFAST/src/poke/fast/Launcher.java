d6b9acd71e1d5ac2d28affe5cfc9db8b841af323
Wajeeh <wajeehmisbahkhan@yahoo.com>
2018-03-19 20:15:13 +0500

    Added Master comment

--- PokeFAST/src/poke/fast/Launcher.java
+++ PokeFAST/src/poke/fast/Launcher.java
@@ -2,6 +2,6 @@ package poke.fast;
 
 public class Launcher {
 	public static void main (String[] args) {
-		System.out.println("Starting Ceremony. Yay!");
+		System.out.println("Starting Ceremony. Master!");
 	}
 }

fc25d63306b5a77e2bce2d478dd7cb2ffc80a89e
wajeehmisbahkhan <37481915+wajeehmisbahkhan@users.noreply.github.com>
2018-03-19 19:53:03 +0500

    Update Launcher.java

--- PokeFAST/src/poke/fast/Launcher.java
+++ PokeFAST/src/poke/fast/Launcher.java
@@ -3,5 +3,6 @@ package poke.fast;
 public class Launcher {
 	public static void main (String[] args) {
 		System.out.println("Starting Ceremony. Yay!");
+		System.out.println("A seperate idea.");
 	}
 }

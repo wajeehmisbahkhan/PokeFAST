92788d0694bf88a397b9ca2b7fd279847ab956c4
Wajeeh <wajeehmisbahkhan@yahoo.com>
2018-03-19 12:30:21 +0500

    Edited message.

--- PokeFAST/src/poke/fast/Launcher.java
+++ PokeFAST/src/poke/fast/Launcher.java
@@ -2,6 +2,6 @@ package poke.fast;
 
 public class Launcher {
 	public static void main (String[] args) {
-		System.out.println("Starting Ceremony.");
+		System.out.println("Starting Ceremony. Yay!");
 	}
 }

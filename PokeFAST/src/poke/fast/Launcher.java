8c8a69e10e5c855e2ac509eb3376adb5e375630a
Bilal-Pc <Bilal-Pc@BILAL-PC>
2018-03-20 10:48:34 +0500

    Message

--- PokeFAST/src/poke/fast/Launcher.java
+++ PokeFAST/src/poke/fast/Launcher.java
@@ -3,7 +3,6 @@ package poke.fast;
 public class Launcher {
 	public static void main (String[] args) {
 		System.out.println("Starting Ceremony. Master!");
-		System.out.println("A seperate idea.");
-		System.out.println("Starting Ceremony. Master!");
+		System.out.println("A stasdasde idea.");
 	}
 }

3eaf0d95ff326922515774578c69bec129ee4bbb
Bilal-Pc <Bilal-Pc@BILAL-PC>
2018-03-25 14:18:18 +0500

    characters and inanimate template defined, will add more after tiles

---
+++ PokeFAST/src/poke/fast/entities/inanimates/Inanimate.java
@@ -0,0 +1,13 @@
+package poke.fast.entities.inanimates;
+
+import poke.fast.Handler;
+import poke.fast.entities.Entity;
+
+public abstract class Inanimate extends Entity {
+
+	public Inanimate(Handler handler, float x, float y, int width, int height) {
+		super(handler, x, y, width, height);
+		// TODO Auto-generated constructor stub
+	}
+
+}

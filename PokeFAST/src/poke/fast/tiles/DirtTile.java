cffcffe56bb573aaa628ce28141451750bae3b80
Bilal-Pc <Bilal-Pc@BILAL-PC>
2018-03-25 15:16:37 +0500

    tile ki classes bana din

---
+++ PokeFAST/src/poke/fast/tiles/DirtTile.java
@@ -0,0 +1,12 @@
+package poke.fast.tiles;
+
+import poke.fast.gfx.Assets;
+
+public class DirtTile extends Tile {
+
+	public DirtTile( int id) {
+		super(Assets.dirt, id);
+		// TODO Auto-generated constructor stub
+	}
+
+}

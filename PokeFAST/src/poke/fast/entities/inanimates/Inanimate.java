fc1f7011201923f2146fbf6fa8e7308d5182db36
Bilal-Pc <Bilal-Pc@BILAL-PC>
2018-03-26 19:37:35 +0500

    Tree stopped moving after dum durood

--- PokeFAST/src/poke/fast/entities/inanimates/Inanimate.java
+++ PokeFAST/src/poke/fast/entities/inanimates/Inanimate.java
@@ -7,7 +7,7 @@ public abstract class Inanimate extends Entity {
 
 	public Inanimate(Handler handler, float x, float y, int width, int height) {
 		super(handler, x, y, width, height);
-		// TODO Auto-generated constructor stub
+		
 	}
 
 }
